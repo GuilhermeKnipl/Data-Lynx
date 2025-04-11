21cf41c8e604641a50e3b4f8930d1f66bf05476c
guilhermeknipl <guiknipl@gmail.com>
2025-04-11 11:07:56 -0300

    Row counting

--- handler.c
+++ handler.c
@@ -1,14 +1,15 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef enum{
 	FLOAT,
 	INT,
 	CHAR
 }Dtype;
 
+
+// vector datatype specification
 typedef union {
     int* i;
     float* f;
@@ -28,50 +29,67 @@ typedef struct DataFrame{
 }DataFrame ;
 
 
-void DType(Col a){
-	switch (a.dtype){
-	case INT:
-		printf("Integer\n");
-		break;
-	case FLOAT:
-		printf("FLOAT\n");
-		break;
-	case CHAR:
-		printf("CHAR\n");
-		break;
+//Identifying Dtype of cols
+void identify_dtype();
+
+// Allocating Space according to Datatype;
+void allocate_vector(int len, Col *column){  
+	switch(column->dtype){
+		case INT:
+			column->vector.i = (int*)malloc(len*sizeof(int));
+			break;
+		case FLOAT:
+			column->vector.f = (float*)malloc(len*sizeof(float));
+			break;
+		case CHAR:
+			column->vector.s = (char*)malloc(len*sizeof(char));
+			break;
 	}
+	
+};
+
+int csv_len(const char* filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Failed to open file");
+        return -1;
+    }
+
+    int lines = 0;
+    int ch;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            lines++;
+        }
+    }
+    fclose(file);
+    return lines;
 }
 
-// Swich case
-void allocating_vector();
 
 int main(){
 	Col *a = (Col*)malloc(sizeof(Col));
 	Col *b = (Col*)malloc(sizeof(Col));
 	Col *c = (Col*)malloc(sizeof(Col));
 	
+	char *filepath = "debug/Housing.csv";
 
+	int len_csv = csv_len(filepath);
+
+	printf("\n=--%d", len_csv);
+	
 	//a->idx = (int*)malloc(sizeof(int));
 	int len = 2;
 	a->dtype = INT;
 	b->dtype = INT;
 	c->dtype = CHAR;
 
-	switch (a->dtype) {
-		case INT:
-			a->vector.i = (int*)malloc(len*sizeof(int));
-			b->vector.i =(int*) malloc(len*sizeof(int));
-				c->vector.s = (char*)malloc(len*sizeof(char));
-			break;
-		case FLOAT:
-			NULL;
-			break;
-		case CHAR:
-			c->vector.s = (char*)malloc(len*sizeof(char));
-			break;
+	allocate_vector(len_csv, a);
+	allocate_vector(len_csv, b);
+	allocate_vector(len_csv, c);
 
-	
-	};
+		
 
 	*b->vector.i = 44;
 	*a->vector.i = 10;
@@ -91,12 +109,13 @@ int main(){
 			printf("\nidx = %d",df.cols[i].idx);
 			printf("\nvalue = %d", *df.cols[i].vector.i);
 		}else{
-			printf("\n%s", df.cols[i].vector.s);
+			printf("\nidx = %d",df.cols[i].idx);
+			printf("\nvalue = %s\n", df.cols[i].vector.s);
 	}
 	}
 	Col d;
 	d.dtype = INT;
 	
-	DType(d);
+	//DType(d);
 	return 1;
 }
