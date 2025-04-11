99d2265bd849c30defe75f16efde1313067b4ae9
guilhermeknipl <guiknipl@gmail.com>
2025-04-10 23:33:52 -0300

    Dynamic datatype handling adjusts

--- handler.c
+++ handler.c
@@ -2,17 +2,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 typedef enum{
 	FLOAT,
 	INT,
 	CHAR
 }Dtype;
 
+typedef union {
+    int* i;
+    float* f;
+    double* d;
+    char* s;  
+} DVector;
 
 typedef struct{
-	char column_name;
+	char clumn_name;
 	int idx;
 	Dtype dtype;
+	DVector vector;
 }Col;
 
 typedef struct DataFrame{
@@ -34,27 +42,58 @@ void DType(Col a){
 	}
 }
 
-
-
+// Swich case
+void allocating_vector();
 
 int main(){
 	Col *a = (Col*)malloc(sizeof(Col));
 	Col *b = (Col*)malloc(sizeof(Col));
-	//a->idx = (int*)malloc(sizeof(int));
+	Col *c = (Col*)malloc(sizeof(Col));
+	
 
+	//a->idx = (int*)malloc(sizeof(int));
+	int len = 2;
 	a->dtype = INT;
+	b->dtype = INT;
+	c->dtype = CHAR;
+
+	switch (a->dtype) {
+		case INT:
+			a->vector.i = (int*)malloc(len*sizeof(int));
+			b->vector.i =(int*) malloc(len*sizeof(int));
+				c->vector.s = (char*)malloc(len*sizeof(char));
+			break;
+		case FLOAT:
+			NULL;
+			break;
+		case CHAR:
+			c->vector.s = (char*)malloc(len*sizeof(char));
+			break;
+
+	
+	};
+
+	*b->vector.i = 44;
+	*a->vector.i = 10;
+	strcpy(c->vector.s, "Hello");
 	a->idx = 0;
 	b->idx = 1;
+	c->idx = 2;
 
 	DataFrame df;
-	df.cols = (Col*)malloc(2* sizeof(Col));
+	df.cols = (Col*)malloc(3* sizeof(Col));
 	df.cols[0] = *a;
 	df.cols[1] = *b;
+	df.cols[2] = *c;
 
-	for(int i = 0; i < 2; i++){
-		printf("%d",df.cols[i].idx);
+	for(int i = 0; i < 3; i++){
+		if (i!=2){
+			printf("\nidx = %d",df.cols[i].idx);
+			printf("\nvalue = %d", *df.cols[i].vector.i);
+		}else{
+			printf("\n%s", df.cols[i].vector.s);
 	}
-
+	}
 	Col d;
 	d.dtype = INT;
 	
