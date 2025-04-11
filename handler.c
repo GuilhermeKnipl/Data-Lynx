43de1f7b86b65d89f57337bcf467d3132a4d12d5
guilhermeknipl <guiknipl@gmail.com>
2025-04-10 22:03:28 -0300

    Basic Dataframe handler

--- handler.c
+++ handler.c
@@ -1,6 +1,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 typedef enum{
 	FLOAT,
 	INT,
@@ -14,12 +15,12 @@ typedef struct{
 	Dtype dtype;
 }Col;
 
-typedef DataFrame{
-	
-} ;
+typedef struct DataFrame{
+	Col* cols;	
+}DataFrame ;
 
 
-void test(Col a){
+void DType(Col a){
 	switch (a.dtype){
 	case INT:
 		printf("Integer\n");
@@ -37,9 +38,26 @@ void test(Col a){
 
 
 int main(){
+	Col *a = (Col*)malloc(sizeof(Col));
+	Col *b = (Col*)malloc(sizeof(Col));
+	//a->idx = (int*)malloc(sizeof(int));
+
+	a->dtype = INT;
+	a->idx = 0;
+	b->idx = 1;
+
+	DataFrame df;
+	df.cols = (Col*)malloc(2* sizeof(Col));
+	df.cols[0] = *a;
+	df.cols[1] = *b;
+
+	for(int i = 0; i < 2; i++){
+		printf("%d",df.cols[i].idx);
+	}
+
 	Col d;
 	d.dtype = INT;
 	
-	test(d);
+	DType(d);
 	return 1;
 }
