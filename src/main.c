018431aec5ed7747684a4859d5ee66f0902ca1ef
GuilhermeKnipl <guirissatti@gmail.com>
2025-04-15 13:33:08 -0300

    Working on data allocation in DataFrame struct

--- src/main.c
+++ src/main.c
@@ -8,40 +8,40 @@
 #include "../include/unit_test.h"
 #include "../include/handler.h"
 
+
+void check_fhealth(FILE *file){
+	if (file == NULL){
+		printf("EMPTY FILE");
+	}else{
+		printf("\nFile Readed\n");
+	}
+}
+
 void file_printer(){
 
-	FILE *file = fopen("debug/Housing.csv", "r");
+	FILE *file = fopen(HOUSING_CSV, "r");
 
+	check_fhealth(file);
 
-
-	if (file == NULL){
-		printf("empty file\n");
-	}else{
-		printf("Readed\n");
-	}
 	char line[256];
 
-	int a = 0;
-	int b = 0;
+	int row  = 0;
+	int col = 0;
 	int columns_iter = 0;
 	while (fgets(line, sizeof(line), file)) {
-		a++ ;
-		b = 0;
+		row++ ; // Line Index
+		col = 0;
 		columns_iter++ ;
 		printf("--\nNew line--");
 		char* tok = strtok(line,",");
 		while (tok != NULL){
-			printf("\n%s",tok);
+			//printf("\nrow: %d col: %d value: %s", row , col , tok);
 			tok = strtok(NULL, ",");
+			col++;
 		}
-		if(a == 3){break;}
+		if(row == 3){break;}
 	}
-
-
-
 }
-
-typedef char *String[] ;
 
 typedef struct Datetime{
 	int day;
@@ -51,15 +51,67 @@ typedef struct Datetime{
 	int minutes;
 }Datetime;
 
+int collen_csv(FILE * file){
 
+	char lsize[256];
+	int row = 0, colidx = 0;
 
+	while (fgets(lsize, sizeof(lsize), file)) {
+			char * tok = strtok(lsize,",");
+		while (tok != NULL) {
+			printf("\nColumn index: %d Value: %s\n", colidx,tok);
+			tok = strtok(NULL,",");
+			colidx++;
+		}
+		row++;
+		if(row == 1){break;};
+
+	    };
+    int colsize = colidx + 1;
+	return colsize;
+}
+
+void ias(){
+
+	FILE *file = fopen(HOUSING_CSV, "r");
+
+	if (!file) {
+		perror("Failed to open file");
+    	}
+
+	int col_len = collen_csv(file);
+	int rows_len = csv_len(file);
+
+	DataFrame * df = (DataFrame*)malloc(sizeof(DataFrame));
+		
+	
+	df->cols = malloc(sizeof(Col)*col_len);
+}	
 
 int main(){
-	test_dtypehandler();	
-
-	dtypehandler("1102");
 
 
+//	test_dtypehandler();		
+	
+	DataFrame df;
+	df.cols = (Col*)malloc(sizeof(int)*12);
+
+	char *val = "hello";	
+	Col *b = (Col*)malloc(sizeof(Col));
+	
+	b->dtype = dtypehandler(val);
+	
+		
+        allocate_vector(100, b);	
+	b->idx = 0;
+	b->vector.s[0] = strdup("hello");
+	b->vector.s[1] = strdup("hello2");
+
+	printf("\nIndex is: %d", b->idx);
+	printf("\nVal %s", b->vector.s[0]);
+	printf("\nVal %s", b->vector.s[1]);
+
+	file_printer();
 	printf("\n");
 	return 0;
 }
